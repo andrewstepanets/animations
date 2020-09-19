29e4fa19b66880c15f33c06598434bfaf47b6353
Andrii Stepanets <ovrass@gmail.com>
2020-09-19 23:27:54 +0300

    using GSAP utils toArray

--- 04-project-pen/js/main.js
+++ 04-project-pen/js/main.js
@@ -24,23 +24,60 @@ function init(){
         }
     });
 
+
+    const partTopOffsets = [547, 722, 842];
+
+    // using GSAP utils toArray
+
     // Part 6 will be  -842px - translateY
     // Part 5  = -722px
     // Part 4 = -547px
 
-    gsap.set('.part4', {y: '-547px'});
-    gsap.set('.part5', {y: '-722px'});
-    gsap.set('.part6', {y: '-842px'});
+    gsap.utils
+        .toArray(['.part4', '.part5', '.part6'])
+        .forEach((part, index) => {
+            gsap.set(part, { y: -partTopOffsets[index]});
+
+            gsap.to(part, {
+                y: 0, ease: 'none', scrollTrigger: {
+                    trigger: '.pen-body',
+                    start: 'top bottom-=640px',
+                    end: `+=${partTopOffsets[index]}`,
+                    scrub: true,
+                    markers: true
+                }
+            });
+        });
+
+    
+
+    // gsap.set('.part4', {y: '-547px'});
+    // gsap.set('.part5', {y: '-722px'});
+    // gsap.set('.part6', {y: '-842px'});
 
     // tween the tip of the pen back to 0
 
-    gsap.to('.part6', {y: 0, ease: 'none', scrollTrigger: {
-        trigger: '.pen-body',
-        start: 'top bottom-=640px',
-        end: '+=842px',
-        scrub: true,
-        markers: true
-    }});
+    // gsap.to('.part6', {y: 0, ease: 'none', scrollTrigger: {
+    //     trigger: '.pen-body',
+    //     start: 'top bottom-=640px',
+    //     end: '+=842px',
+    //     scrub: true,
+    //     markers: true
+    // }});
+    // gsap.to('.part5', {y: 0, ease: 'none', scrollTrigger: {
+    //     trigger: '.pen-body',
+    //     start: 'top bottom-=640px',
+    //     end: '+=722px',
+    //     scrub: true,
+    //     markers: true
+    // }});
+    // gsap.to('.part4', {y: 0, ease: 'none', scrollTrigger: {
+    //     trigger: '.pen-body',
+    //     start: 'top bottom-=640px',
+    //     end: '+=547px',
+    //     scrub: true,
+    //     markers: true
+    // }});
 
 }
 
