e5fb8eef2b30b391bbf3632856f17d56ce642d1e
Andrii Stepanets <ovrass@gmail.com>
2020-09-19 17:30:30 +0300

    Reset bottom parts of the pen

--- 04-project-pen/js/main.js
+++ 04-project-pen/js/main.js
@@ -1,22 +1,41 @@
 gsap.registerPlugin(ScrollTrigger);
 
+const getTopPartsHeight = () => {
+   return document.querySelector('.pen-top').clientHeight - 22;
+};
+
+
 function init(){
     
     // move part 3 to cover 2
-    const penTopHeight = document.querySelector('.pen-top').clientHeight;
+    // const penTopHeight = document.querySelector('.pen-top').clientHeight;
 
     gsap.set('.part3', {
         y: () => {
-            return -(penTopHeight  - 22);
+            return -(getTopPartsHeight());
         },
         scrollTrigger: {
             id: 'pen-body',
             trigger: '.part3',
             start: 'top bottom-=270px',
-            end: `+=${penTopHeight - 22}`,
+            end: `+=${getTopPartsHeight()}`,
             pin: true,
-            markers: true
+            pinSpacing: false,
         }
+    });
+
+    // Part 6 will be  -842px - translateY
+    // Part 5  = -722px
+    // Part 4 = -547px
+
+    gsap.set('.part4', {
+        y: '-547px'
+    });
+    gsap.set('.part5', {
+        y: '-722px'
+    });
+    gsap.set('.part6', {
+        y: '-842px'
     });
 
 }
